aa3a452a76f1716ea837f58a66737d0ad57caa9b
Aryan Gupta <aryangupta07075@gmail.com>
2023-02-01 22:47:51 +0000

    Styling

--- main.c
+++ main.c
@@ -9,35 +9,42 @@ int errors(char text[], char user_input[]);
 float gross_wpm(int len_text, float total_time);
 float net_wpm(int total_charac_by_user, int error, float total_time);
 float accuracy(float net_wpm, float gross_wpm);
+void display_text(char string[]);
+void display_result(int error, float Gross_wpm, float Net_wpm, float Accuracy, int length_user_input);
 
+
+// main 
 int main()
 {
     float Gross_wpm, total_time, Net_wpm, Accuracy;
-    int length, length_user_input, error;  
+    int length, length_user_input, error, i = 1;
+  
     //Text contains the string that the user has to enter  
     // Test string     
-    char text_display[1000] = "A definite article is an article that marks a definite noun phrase. Definite articles such as English the are used to refer to a particular member of a group. It may be something that the speaker has already mentioned or it may be otherwise something uniquely specified."
+    char text[1000] = "A definite article is an article that marks a definite noun phrase. Definite articles such as English the are used to refer to a particular member of a group. It may be something that the speaker has already mentioned or it may be otherwise something uniquely specified."
     "For example, Sentence 1 uses the definite article and thus expresses a request for a particular book. In contrast, Sentence 2 uses an indefinite article and thus conveys that the speaker would be satisfied with any book.";
-    char user_input[1000];
-    printf("%s\n", text_display);
+    
     // user_input contains the user' s response       
+    char user_input[1000];
+    display_text(text);
     
-    // printf("%s\n", text);
-
-    
-
     gets(user_input);
     length = strlen(user_input);
-    // printf("%s\n", user_input);
     
     total_time = 1;
     length_user_input = total_charac_by_user(user_input);
     Gross_wpm = gross_wpm(length_user_input, total_time);
-    error = errors(text_display, user_input);
-    
-    
+    error = errors(text, user_input);
     Net_wpm = net_wpm(length_user_input, error, total_time);
     Accuracy = accuracy(Net_wpm, Gross_wpm);
+    
+    display_result(error, Gross_wpm, Net_wpm, Accuracy, length_user_input);
+ 
+}
+
+// Function to calculate and display result of the user
+void display_result(int error, float Gross_wpm, float Net_wpm, float Accuracy, int length_user_input)
+{
     if (length_user_input < 20)
     {
         printf("Insufficient data to analyze. Please provide sufficient data to get accurate result\n");
@@ -52,6 +59,11 @@ int main()
     }
     
 
+}
+// Function to display the text for the user 
+void display_text(char text[])
+{
+    printf("%s\n", text);
 }
 
 // Function to calculate the total characters by the user except the spaces 
